fa3c05eb605cc0fde512e16975cdd0cf54cc3838
lichessapps <lichessapps@gmail.com>
2018-03-07 12:27:02 +0100

    App

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
-const app = express();
 const PORT = process.env.PORT || 5000;
 // server startup
+const app = express();
 app.use(morgan('combined'));
 app.use(express.static('server/assets'));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'server/pages/index.html')));
--- server/head.ts
+++ server/head.ts
@@ -4,7 +4,6 @@
 const express = require('express')
 const morgan = require('morgan')
 const path = require('path')
-const app = express()
 
 const PORT = process.env.PORT || 5000
 
